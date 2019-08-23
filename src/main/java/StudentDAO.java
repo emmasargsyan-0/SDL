376279e098f31c8b0e2ec9a6dd833c9f98aa4a89
HER2019 <raf.sus@mail.ru>
2019-08-23 12:50:15 +0400

    Added get students funtion

--- src/main/java/StudentDAO.java
+++ src/main/java/StudentDAO.java
@@ -2,12 +2,18 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class StudentDAO implements DAO<Student>{
-    private Connection connection ;
-    StudentDAO(Connection connection){
+
+    private Connection connection;
+
+    public StudentDAO(Connection connection){
         this.connection = connection;
     }
+
     public Student get(String id){
         return null;
     }
@@ -31,6 +37,27 @@ public class StudentDAO implements DAO<Student>{
 
     public boolean remove(Student obj){
         return true;
+    }
+
+    public Map allStudentList(){
+        Map students = new HashMap();
+        try {
+            String query="SELECT Students.[Name], AVG(CAST(Grades.Grade as float)) AS Grade \n" +
+                    "FROM Students\n" +
+                    "JOIN Grades on Students.Id=Grades.StudentId\n" +
+                    "GROUP BY Students.[Name]";
+            PreparedStatement statement = connection.prepareStatement(query);
+            ResultSet resultSet = statement.executeQuery();
+
+            while (resultSet.next()) {
+                String name = resultSet.getString("Name");
+                String grade = resultSet.getString("Grade");
+                students.put(name, grade);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return students;
     }
 
 }
